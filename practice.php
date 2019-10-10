d83a4207c42b5c218544923548cf3cd9437da51a
”KANEKO <ukowtimety.eringi184@gmail.com>
2019-10-10 08:28:36 +0000

    課題です。

---
+++ practice.php
@@ -0,0 +1,13 @@
+<?php
+$a=3;
+$b=7;
+echo $a + $b;
+$array=["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
+echo $array[7];
+$hello="Hello,";
+$name="TAKAKI";
+$world="'s World!";
+$greeting = $hello .$name .$world;
+echo $greeting;
+$tech_boost="tech";
+echo $tech_boost . "boost";
